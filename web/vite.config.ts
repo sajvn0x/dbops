4c6d2d6ff74bb4905e80d84bc7f2dfc9ac4bc31b
sajeevan <sajeevanjspy@gmail.com>
2026-03-04 23:53:35 +0530

    init: shadcn ui

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,16 @@
+import path from "node:path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({ 
+export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	server: {
 		port: 3000
 	},
